b50003837b240027ddbfac7e92fd8a8bb7493eaf
Zhe <zzsjtu1988@163.com>
2020-03-12 23:23:05 +0800

    Add files via upload

---
+++ chap3.py
@@ -0,0 +1,22 @@
+## 第一题
+a = 'hello,world'
+print(a)
+b = 1.414
+print(b)
+c = (1 != 2)
+print(c)
+d = (True and False)
+print(d)
+e = (True or False)
+print(e)
+
+## 第二题
+print("a:%s; b:%.2f; c:%s; d:%s; e:%s" % (a, b, c, d, e))
+
+## 第三题
+s = '''
+'hello world'
+\\"hello,world"
+'''
+
+print(s)
